29447ab798ac5cd3654a545004f80ab8338ed4d3
Kay <kaiying001@gmail.com>
2020-05-03 19:02:07 +0800

    Initial Commit for Harvest Behaviour of Farmer

---
+++ src/game/HarvestBehaviour.java
@@ -0,0 +1,31 @@
+package game;
+
+import edu.monash.fit2099.engine.Action;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+
+public class HarvestBehaviour extends Action implements Behaviour {
+
+
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        int x = map.locationOf(actor).x();
+        int y = map.locationOf(actor).y();
+
+        if(map.locationOf(actor).getDisplayChar() == 'C'){
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        return actor.toString() + "harvests a crop";
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return "";
+    }
+}
