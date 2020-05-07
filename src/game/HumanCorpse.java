49c58f038caa791c7afcb29254f2c82c7c341b30
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2020-05-08 01:13:21 +0800

    Initial commit message for HumanCorpse

---
+++ src/game/HumanCorpse.java
@@ -0,0 +1,49 @@
+package game;
+
+import edu.monash.fit2099.engine.Display;
+import edu.monash.fit2099.engine.Item;
+import edu.monash.fit2099.engine.Location;
+
+import java.util.Random;
+
+public class HumanCorpse extends Item {
+    private int turn;
+    private Random rand = new Random();
+
+    public HumanCorpse(String name){
+        super(name, '%', false);
+    }
+
+    @Override
+    public void tick(Location currentLocation) {
+        boolean revive = rand.nextBoolean();
+        super.tick(currentLocation);
+
+        turn++;
+
+        if((turn >= 5 && turn < 10 && revive) || turn == 10){
+            corpseRevive(currentLocation);
+        }
+    }
+
+    public void corpseRevive(Location here){
+        Display display = new Display();
+        String retVal;
+
+        // Create a new zombie
+        here.addActor(new Zombie("Zombie " + this.name));
+
+        // Display message
+        retVal = this.name + " has turned into Zombie.\n" ;
+
+        // Remove the corpse
+        for(Item item: here.getItems()){
+            if(item.getDisplayChar() == '%'){
+                here.removeItem(item);
+                break;
+            }
+        }
+
+        display.println(retVal);
+    }
+}
