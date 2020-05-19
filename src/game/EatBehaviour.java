f1aba565eb48623ee15d6926378cb9094cd9b184
ktey0001 <ktey0001@student.monash.edu>
2020-05-19 22:57:02 +0800

    Code changes: check if the item has food capability: Edible instead of getting display character, return a new eat action

--- src/game/EatBehaviour.java
+++ src/game/EatBehaviour.java
@@ -2,27 +2,19 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
-public class EatBehaviour extends Action implements Behaviour {
+
+
+public class EatBehaviour implements Behaviour {
+
     @Override
     public Action getAction(Actor actor, GameMap map) {
         for(Item item : actor.getInventory()){
-            if(item.getDisplayChar() == 'E'){
-                actor.heal(((Food) item).getFoodValue());
-                actor.removeItemFromInventory(item);
-                return this;
+            if(item.hasCapability(Food.FoodCapability.EDIBLE)){
+                return new EatAction((Food)item);
             }
 
         }
         return null;
     }
 
-    @Override
-    public String execute(Actor actor, GameMap map) {
-        return actor.toString() + " eats and heals";
-    }
-
-    @Override
-    public String menuDescription(Actor actor) {
-        return "";
-    }
 }
