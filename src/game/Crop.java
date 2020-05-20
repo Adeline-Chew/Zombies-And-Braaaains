af1b52cb02cda301b4b6b040de671ce2e6e2e287
ktey0001 <ktey0001@student.monash.edu>
2020-05-20 17:11:25 +0800

    Java documentation for Crop class

--- src/game/Crop.java
+++ src/game/Crop.java
@@ -3,15 +3,29 @@ package game;
 import edu.monash.fit2099.engine.Ground;
 import edu.monash.fit2099.engine.Location;
 
+/**
+ * A crop sowed by Farmer that will grow to become food
+ *
+ * @author Tey Kai Ying
+ */
+
 public class Crop extends Ground {
     private int turn = 0;
 
+    /**
+     * Constructor. Crop has capability UNRIPE upon construction.
+     *
+     */
     public Crop() {
         super('c');
         this.addCapability(CropCapability.UNRIPE);
     }
 
-
+    /**
+     * Crop will grow and ripe after 20 turns. A capability RIPEN will be added.
+     *
+     * @param currentLocation location of the ground
+     */
      @Override
     public void tick(Location currentLocation){
         super.tick(currentLocation);
@@ -21,17 +35,20 @@ public class Crop extends Ground {
         if(turn == 20){
             displayChar = 'C';
             this.addCapability(CropCapability.RIPEN);
-        }
 
-        if(turn > 20){
-            displayChar = 'C';
         }
 
      }
 
-     @Override
+    /**
+     * Decrease the turns of the crop to ripe by 10 turns.
+     * 
+     */
      public void speedGrowth(){
-        if(turn < 20){
+        if(turn + 10 >= 20){
+            turn = 20;
+        }
+        else{
             turn += 10;
         }
      }
