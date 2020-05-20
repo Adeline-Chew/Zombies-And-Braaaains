8ed7c9245dc76951a13e0b8fee684305de476b76
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2020-05-21 01:05:56 +0800

    Java documentation for HumanCorpse

--- src/game/HumanCorpse.java
+++ src/game/HumanCorpse.java
@@ -6,14 +6,34 @@ import edu.monash.fit2099.engine.Location;
 
 import java.util.Random;
 
+/**
+ * A Human corpse.
+ *
+ * Human corpse will return from dead within 5 - 10 turns.
+ *
+ * @author Adeline Chew Yao Yi, Tey Kai Ying
+ *
+ */
+
 public class HumanCorpse extends PortableItem {
     private int turn = 0;
     private Random rand = new Random();
 
+    /**
+     * A Constructor creates human corpse.
+     * @param name Display name of the dead Human.
+     */
     public HumanCorpse(String name){
         super(name, '%');
     }
 
+    /**
+     * Count the turn of the game.
+     * After 5 turns, the corpse has 50% of chance to turn into Zombie.
+     * In 10th turn, the corpse must be turned into Zombie.
+     *
+     * @param currentLocation The location of the ground on which we lie.
+     */
     @Override
     public void tick(Location currentLocation) {
         boolean revive = rand.nextBoolean();
@@ -24,9 +44,16 @@ public class HumanCorpse extends PortableItem {
             corpseRevive(currentLocation);
             currentLocation.removeItem(this);
         }
-
     }
 
+    /**
+     * Count the turn of the game when the corpse is carrying by an actor.
+     * After 5 turns, the corpse has 50% of chance to turn into Zombie.
+     * In 10th turn, the corpse must be turned into Zombie.
+     *
+     * @param currentLocation The location of the actor carrying this Item.
+     * @param actor The actor carrying this Item.
+     */
     @Override
     public void tick(Location currentLocation, Actor actor){
         boolean revive = rand.nextBoolean();
@@ -39,6 +66,10 @@ public class HumanCorpse extends PortableItem {
         }
     }
 
+    /**
+     * Turn the HumanCorpse into Zombie at a specified location.
+     * @param here Location of where the HumanCorpse turns into Zombie.
+     */
     private void corpseRevive(Location here){
         Display display = new Display();
         String retVal;
