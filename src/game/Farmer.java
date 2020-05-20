45695ea9e2f76b3be43479a8183f9381ac0d9fc2
ktey0001 <ktey0001@student.monash.edu>
2020-05-20 17:23:09 +0800

    Farmer has new behaviours : PickUpBehaviour and EatBehaviour

--- src/game/Farmer.java
+++ src/game/Farmer.java
@@ -8,16 +8,22 @@ import edu.monash.fit2099.engine.*;
  * This farmer has same characteristics as human, but also
  * has the abilities to sow a crop, fertilise a crop and harvest a crop.
  *
- * @author Tey Kai Ying
+ * @author Adeline Chew Yao Yi and Tey Kai Ying
  *
  */
 
 public class Farmer extends Human {
 
-    private Behaviour[] behaviours = {new HarvestBehaviour(), new FertiliseBehaviour(), new SowBehaviour(), new WanderBehaviour()};
+    private Behaviour[] behaviours = {
+            new EatBehaviour(),
+            new FertiliseBehaviour(),
+            new HarvestBehaviour(),
+            new SowBehaviour(),
+            new PickUpBehaviour(),
+            new WanderBehaviour()};
 
     /**
-     * The default constructor creates default Humans
+     * The constructor creates default Farmer
      *
      * @param name the Farmer's display name
      */
@@ -26,7 +32,8 @@ public class Farmer extends Human {
     }
 
     /**
-     * Firstly check if the Farmer stands on a ripen crop. If not, check if the Farmer stands on an unripe crop.
+     * Firstly check if the Farmer is damaged. If yes, it will has a new capability.
+     * Check if the Farmer stands on a unripe crop. If not, check if the Farmer stands on an ripen crop.
      * If not both, the Farmer will sow a crop while standing on a dirt depends on the probability.
      * If no crops or dirt are close enough it will wander randomly.
      *
@@ -36,6 +43,11 @@ public class Farmer extends Human {
      * @param display the Display where the Farmer's utterances will be displayed
      */
     public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+        // if this human has lower health points, it should be considered as damaged
+        if(this.hitPoints < this.maxHitPoints && !this.hasCapability(ZombieCapability.DAMAGED)){
+            this.addCapability(ZombieCapability.DAMAGED);
+        }
+
         for (Behaviour behaviour : behaviours) {
             Action action = behaviour.getAction(this, map);
             if (action != null)
