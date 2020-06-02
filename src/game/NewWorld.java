969b09f1f73d18676afac156fa858792f59d9960
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2020-06-02 15:46:06 +0800

    New class: NewWorld

---
+++ src/game/NewWorld.java
@@ -0,0 +1,15 @@
+package game;
+
+import edu.monash.fit2099.engine.Display;
+import edu.monash.fit2099.engine.World;
+
+public class NewWorld extends World {
+    /**
+     * Constructor.
+     *
+     * @param display the Display that will display this World.
+     */
+    public NewWorld(Display display) {
+        super(display);
+    }
+}
