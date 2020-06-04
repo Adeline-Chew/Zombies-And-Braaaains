cb207bb555563574421b6555334fbd070eac1f90
ktey0001 <ktey0001@student.monash.edu>
2020-06-04 14:01:59 +0800

    Java documentation for Vehicle class

--- src/game/Vehicle.java
+++ src/game/Vehicle.java
@@ -4,7 +4,18 @@ import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.MoveActorAction;
 
+/**
+ * An item for Player to move between town map and compound map
+ *
+ */
+
 public class Vehicle extends Item {
+
+    /**
+     * Constructor. The vehicle is not portable.
+     *
+     * @param map destination map
+     */
 
     public Vehicle(GameMap map){
         super("Tesla", 'V', false);
