70b9ae6639cdfe3599689fe8b1a0d786669bde7b
ktey0001 <ktey0001@student.monash.edu>
2020-06-04 17:09:39 +0800

    Java documentation for QuitGameAction class

--- src/game/QuitGameAction.java
+++ src/game/QuitGameAction.java
@@ -4,14 +4,32 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * Action to allow the Player to quit the game.
+ *
+ * @author Adeline Chew Yao Yi and Tey Kai Ying
+ */
 public class QuitGameAction extends Action {
 
+    /**
+     * Remove player from the map
+     *
+     * @param actor The actor performing the action.
+     * @param map The map the actor is on.
+     * @return a String of the action suitable for feedback in the UI.
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         map.removeActor(actor);
         return "Player quits the game.";
     }
 
+    /**
+     * Returns a description of this action suitable to display in the menu.
+     *
+     * @param actor The actor performing the action.
+     * @return a string description about quit action
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " quit the game";
