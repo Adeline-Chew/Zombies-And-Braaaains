4d49a4f97dc668c46fe0a4708cdfdc4e63ea2f0c
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2020-06-06 23:48:36 +0800

    Initial commit message of ChooseWeaponAction: Player can choose which ranged weapon as weapon

---
+++ src/game/ChooseWeaponAction.java
@@ -0,0 +1,37 @@
+package game;
+
+import edu.monash.fit2099.engine.*;
+
+public class ChooseWeaponAction extends Action {
+    RangedWeapon weapon;
+    Display display;
+
+    public ChooseWeaponAction(Display display, RangedWeapon weapon){
+        this.weapon = weapon;
+        this.display = display;
+    }
+    /**
+     * Perform the Action.
+     *
+     * @param actor The actor performing the action.
+     * @param map   The map the actor is on.
+     * @return a description of what happened that can be displayed to the user.
+     */
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        Action action = weapon.subMenu(actor, map, display);
+        return action.execute(actor, map);
+    }
+
+    /**
+     * Returns a descriptive string
+     *
+     * @param actor The actor performing the action.
+     * @return the text we put on the menu
+     */
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " use " + weapon.getName();
+    }
+
+}
