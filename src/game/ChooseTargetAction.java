8c272b72cac42f4270f24be34f3e2388c6646bfe
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2020-06-07 02:16:20 +0800

    Conditions added in execute() in ChooseTargetAction

--- src/game/ChooseTargetAction.java
+++ src/game/ChooseTargetAction.java
@@ -4,12 +4,12 @@ import edu.monash.fit2099.engine.*;
 
 public class ChooseTargetAction extends Action {
     Actor target;
-    RangedWeapon sniper;
+    SniperRifle sniper;
     AimAction aim;
     Menu menu = new Menu();
     Display display;
 
-    public ChooseTargetAction(Actor actor, RangedWeapon weapon, Display display){
+    public ChooseTargetAction(Actor actor, SniperRifle weapon, Display display){
         this.target = actor;
         this.sniper = weapon;
         this.aim = new AimAction(target);
@@ -26,10 +26,26 @@ public class ChooseTargetAction extends Action {
     @Override
     public String execute(Actor actor, GameMap map) {
         Actions actions = new Actions();
-        actions.add(aim);
-        actions.add(new RangedAttackAction(sniper, target));
+        String result;
+        if(this.aim.getConcentration() >= 0 && this.aim.getConcentration() < 2)
+            actions.add(aim);
+        actions.add(new RangedAttackAction(sniper, target, getShootProbability(), sniper.getSniperDamage().get(getShootProbability())));
+        Action action = menu.showMenu(actor, actions, display);
+        result = action.execute(actor, map);
+        if(action instanceof AimAction)
+            Player.concentrateAction(this, aim.getConcentration());
 
-        return menu.showMenu(actor, actions, display).execute(actor, map);
+        return result;
+    }
+
+    private double getShootProbability(){
+        double prob = 1;
+        int concentration = this.aim.getConcentration();
+        if(concentration == 0)
+            prob = 0.75;
+        else if(concentration == 1)
+            prob = 0.90;
+        return prob;
     }
 
     /**
