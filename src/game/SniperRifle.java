d75feb643f5808e6fd988f8caa4e2bd0233298c4
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2020-06-07 02:12:28 +0800

    Commit message for SniperRifle: Added HashMap to link sniper's damage and its probability

--- src/game/SniperRifle.java
+++ src/game/SniperRifle.java
@@ -6,13 +6,16 @@ import edu.monash.fit2099.engine.*;
 import java.util.*;
 
 public class SniperRifle extends RangedWeapon {
-    SniperAmmunition[] sniperAmmunition;
-    int range = 10;
+    private SniperAmmunition[] sniperAmmunition;
+    private final HashMap<Double, Integer> sniperDamage = new HashMap<>();
 
     public SniperRifle(){
         super("Sniper Rifle", 'S', 20, "fires");
         sniperAmmunition = new SniperAmmunition[20];
         this.addCapability(ItemCapability.RANGED_WEAPON);
+        sniperDamage.put(0.75, 20);
+        sniperDamage.put(0.90, 40);
+        sniperDamage.put(1.00, 200);
     }
 
     @Override
@@ -36,6 +39,7 @@ public class SniperRifle extends RangedWeapon {
         ArrayList<ArrayList<Location>> layer = new ArrayList<>();
         layer.add(now);
 
+        int range = 10;
         for(int r = 0; r < range; r++){
             layer = getNextLayer(actor, layer, visitedLocations);
             targets.addAll(search(layer));
@@ -79,6 +83,10 @@ public class SniperRifle extends RangedWeapon {
     @Override
     public void lostAmmunition() {
 
+    }
+
+    public HashMap<Double, Integer> getSniperDamage(){
+        return sniperDamage;
     }
 
 }
