7ab93b856f823b9399bce95ce934610cd93e0136
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2020-06-07 02:17:00 +0800

    Changed the parent class of Shotgun and implemented the abstract method.

--- src/game/Shotgun.java
+++ src/game/Shotgun.java
@@ -1,15 +1,26 @@
 package game;
 
-import edu.monash.fit2099.engine.WeaponItem;
+import edu.monash.fit2099.engine.*;
 
-public class Shotgun extends WeaponItem {
+public class Shotgun extends RangedWeapon {
     final String [] DIRECTIONS = {"North", "North-East", "East", "South-East", "South", "South-West", "West", "North-West"};
 
     public Shotgun() {
         super("Shotgun", 'G', 20, "fires");
-        addCapability(ItemCapability.SHOTGUN);
-        for(String direction : DIRECTIONS)
-            this.allowableActions.add(new ShotDirectionAction(direction));
+        addCapability(ItemCapability.RANGED_WEAPON);
     }
 
+    @Override
+    public Action subMenu(Actor actor, GameMap map, Display display) {
+        Actions actions = new Actions();
+        for(String direction : DIRECTIONS){
+            actions.add(new ShotDirectionAction(this, direction));
+        }
+        return menu.showMenu(actor, actions, display);
+    }
+
+    @Override
+    public void lostAmmunition() {
+
+    }
 }
