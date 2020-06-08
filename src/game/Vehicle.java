6369a4f37772d8851b1732efa5f24daba74aa2b3
Adeline Chew Yao Yi <ache0072@student.monash.edu>
2020-06-09 02:07:01 +0800

    Create a method to randomly get a location near the vehicle when moving player.

--- src/game/Vehicle.java
+++ src/game/Vehicle.java
@@ -1,8 +1,10 @@
 package game;
 
-import edu.monash.fit2099.engine.GameMap;
-import edu.monash.fit2099.engine.Item;
-import edu.monash.fit2099.engine.MoveActorAction;
+import edu.monash.fit2099.engine.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * An item for Player to move between town map and compound map
@@ -19,7 +21,25 @@ public class Vehicle extends Item {
 
     public Vehicle(GameMap map){
         super("Tesla", 'V', false);
-        this.allowableActions.add(new MoveActorAction(map.at(39, 11), "toward another map..."));
+        this.allowableActions.add(new MoveActorAction(moveActor(map), "toward another map..."));
+    }
+
+    /**
+     * Get the available adjacent location of the vehicle to move the actor.
+     * @param map GameMap
+     * @return Location without an Actor standing.
+     */
+    private Location moveActor(GameMap map){
+        Location vehicleCo = map.at(40, 11);
+
+        List<Exit> adjacent = new ArrayList<>(vehicleCo.getExits());
+        Collections.shuffle(adjacent);
+
+        for(Exit e : adjacent){
+            if(!e.getDestination().containsAnActor())
+                return e.getDestination();
+        }
+        return vehicleCo;
     }
 
 }
