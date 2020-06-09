c3802649ab0f7e307342c119b407c37e082ad419
ktey0001 <ktey0001@student.monash.edu>
2020-06-09 21:01:53 +0800

    Java documentation for Shotgun

--- src/game/Shotgun.java
+++ src/game/Shotgun.java
@@ -2,13 +2,29 @@ package game;
 
 import edu.monash.fit2099.engine.*;
 
+/**
+ * A ranged weapon.
+ *
+ */
+
 public class Shotgun extends RangedWeapon {
     final String [] DIRECTIONS = {"North", "North-East", "East", "South-East", "South", "South-West", "West", "North-West"};
 
+    /**
+     * Constructor.
+     */
     public Shotgun() {
         super("Shotgun", 'G', 20, "bangs");
     }
 
+    /**
+     * Display the menu of description of actions with 8 different directions
+     *
+     * @param actor actor performs the action
+     * @param map the map where the current actor is
+     * @param display the Display where the weapon's utterances will be displayed
+     * @return an action selected from Player
+     */
     @Override
     public Action subMenu(Actor actor, GameMap map, Display display) {
         Actions actions = new Actions();
