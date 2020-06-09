0b3be7d12b73d95667a5b701a876beac841496f8
ktey0001 <ktey0001@student.monash.edu>
2020-06-09 18:26:39 +0800

    Java documentation for CureBehaviour class

--- src/game/CureBehaviour.java
+++ src/game/CureBehaviour.java
@@ -6,7 +6,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A class that allows a Doctor to cure.
+ *
+ * @author Adeline Chew Yao Yi and Tey Kai Ying
+ *
+ */
+
 public class CureBehaviour implements Behaviour {
+
+    /**
+     * If Doctor is surrounded with a Human, Doctor will present an inject action
+     *
+     * @param actor the Actor acting
+     * @param map the GameMap containing the Actor
+     * @return an Action, or null if no Actor around
+     */
 
     @Override
     public Action getAction(Actor actor, GameMap map) {
@@ -18,7 +33,6 @@ public class CureBehaviour implements Behaviour {
                 return new InjectVaccineAction(e.getDestination().getActor());
             }
         }
-
         return null;
     }
 }
