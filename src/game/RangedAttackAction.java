2d32ff797d831fd1f3475a4759de5cfc14e1af3f
ktey0001 <ktey0001@student.monash.edu>
2020-06-11 20:01:15 +0800

    Java documentation for RangedAttackAction

--- src/game/RangedAttackAction.java
+++ src/game/RangedAttackAction.java
@@ -4,12 +4,27 @@ import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 
+/**
+ * Action for Player to use guns for ranged attack.
+ *
+ * @author Adeline Chew Yao Yi and Tey Kai Ying
+ *
+ */
+
 public class RangedAttackAction extends Action {
     private final RangedWeapon weapon;
     private final Actor target;
     private final double hitProb;
     private final int damage;
 
+    /**
+     * Constructor.
+     *
+     * @param rangedWeapon weapon to be used
+     * @param target targeted actor
+     * @param hitProbability probability of hitting
+     * @param damage weapon's damage
+     */
     public RangedAttackAction(RangedWeapon rangedWeapon, Actor target, double hitProbability, int damage){
         this.weapon = rangedWeapon;
         this.target = target;
@@ -17,6 +32,14 @@ public class RangedAttackAction extends Action {
         this.damage = damage;
     }
 
+    /**
+     * Perform the Ranged Attack Action. Calculate the probability of hitting.
+     * Also check if the target is conscious after being attacked.
+     *
+     * @param actor The actor performing the action.
+     * @param map   The map the actor is on.
+     * @return a description of what happened that can be displayed to the user.
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         String result = actor + " miss " + target;
@@ -32,6 +55,12 @@ public class RangedAttackAction extends Action {
         return result;
     }
 
+    /**
+     * Describe the action to be displayed in the menu
+     *
+     * @param actor The actor performing the action.
+     * @return a an empty string as it is not needed to be shown
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " fires " + target;
