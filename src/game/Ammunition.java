934b4c7f5ff9ccc2c6790f488eaab1a3d1a12c87
ktey0001 <ktey0001@student.monash.edu>
2020-06-11 20:00:41 +0800

    Java documentation for Ammunition

--- src/game/Ammunition.java
+++ src/game/Ammunition.java
@@ -2,8 +2,15 @@ package game;
 
 import edu.monash.fit2099.engine.Item;
 
+/**
+ * Item to represent bullet for guns.
+ */
+
 public class Ammunition extends Item {
 
+    /**
+     * Constructor.
+     */
     public Ammunition() {
         super("Ammunition", 'A', false);
     }
